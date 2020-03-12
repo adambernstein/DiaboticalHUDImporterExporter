8646904c3919251e187e7362d62a4ce201bb10af
Adam Bernstein <adam_bernstein@hms.harvard.edu>
2020-03-11 20:07:38 -0400

    Working Window and filereader!

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,3 +1,14 @@
-﻿Class MainWindow
+﻿Imports System.Environment
+Imports System.Text.RegularExpressions
 
+
+Class MainWindow
+    Private Sub ExportDhud(sender As Object, e As RoutedEventArgs)
+        Dim fileReader As String
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        'Dim HudDefinitionRegex As New Regex("[\t ](?<w>((774)|(Bos)|(Das))[a-z0-9]*)[\t ]", RegexOptions.ExplicitCapture Or RegexOptions.IgnoreCase Or RegexOptions.Compiled)
+        StatusText1.Text = "Exporting...."
+        fileReader = My.Computer.FileSystem.ReadAllText(appData & "\Diabotical\Settings.txt")
+        TextFileContent.Text = fileReader
+    End Sub
 End Class

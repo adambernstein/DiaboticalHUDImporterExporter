9ea5d7080fa006cf402fc11cd730aa6792747537
Adam Bernstein <adam_bernstein@hms.harvard.edu>
2020-03-11 20:50:42 -0400

    Find "hud_definition"

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,14 +1,32 @@
 ﻿Imports System.Environment
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.FileIO.TextFieldParser
 
 
 Class MainWindow
     Private Sub ExportDhud(sender As Object, e As RoutedEventArgs)
-        Dim fileReader As String
-        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
-        'Dim HudDefinitionRegex As New Regex("[\t ](?<w>((774)|(Bos)|(Das))[a-z0-9]*)[\t ]", RegexOptions.ExplicitCapture Or RegexOptions.IgnoreCase Or RegexOptions.Compiled)
         StatusText1.Text = "Exporting...."
-        fileReader = My.Computer.FileSystem.ReadAllText(appData & "\Diabotical\Settings.txt")
-        TextFileContent.Text = fileReader
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        'Dim SettingsFile As String = My.Computer.FileSystem.ReadAllText(appData & "\Diabotical\Settings.txt")
+        Using SettingsFile As New Microsoft.VisualBasic.FileIO.TextFieldParser(appData & "\Diabotical\Settings.txt")
+            SettingsFile.TextFieldType = FileIO.FieldType.Delimited
+            SettingsFile.SetDelimiters("=")
+            Dim currentRow As String()
+            While Not SettingsFile.EndOfData
+                Try
+                    currentRow = SettingsFile.ReadFields()
+                    Dim currentField As String
+                    For Each currentField In currentRow
+                        If currentField = "hud_definition" Then
+                            MsgBox("Found " & currentField)
+                        End If
+                    Next
+                Catch ex As Microsoft.VisualBasic.
+                  FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message &
+        "is not valid and will be skipped.")
+                End Try
+            End While
+        End Using
     End Sub
 End Class
